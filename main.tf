4e0214cc50970bec68bb96ab518f4e99dcd59f6e
ahmedrabie33 <ahmedelezmazy36@gmail.com>
2025-12-21 14:14:11 +0200

    myresources

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
-module "vpc" {
-  source = "../vpc"
+resource "aws_iam_user" "fox" {
+  path = "/"
+  name = "fox"
 
 }
-
-
-
-resource "aws_instance" "ec2v" {
-  ami           = "ami-068c0051b15cdb816"
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc.subnetout
-
-}
-
---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region                   = "us-east-1"
+  profile                  = "terraform_user"
+  shared_config_files      = ["C:/Users/GHOST/.aws/config"]
+  shared_credentials_files = ["C:/Users/GHOST/.aws/credentials"]
+}
+
